56de654366c0fe53ddf69600a197c779b9ba7516
DailyDoseofDogfood <76919179+DailyDoseofDogfood@users.noreply.github.com>
2023-05-20 22:14:11 -0400

    Add files via upload

--- distancemidpointcalculator.py
+++ distancemidpointcalculator.py
@@ -2,9 +2,8 @@ import math
 if __name__ == "__main__":
     print("Distance and Midpoint calculator")
 
-
+    #Find the distance between two points
     def distance():
-        #user defines points
         x1, y1 = input("[Distance] What is the first set of points? Use format: \"x, y\"\n").split(", ")
         x2, y2 = input("[Distance] What is the second set of points? Use format: \"x, y\"\n").split(", ")
         x1 = float(x1)
@@ -12,33 +11,26 @@ if __name__ == "__main__":
         x2 = float(x2)
         y2 = float(y2)
         d = ((x2-x1) ** 2) + ((y2-y1) ** 2)
-        #Prints answer with
         print("The distance between (", x1, ", ", y1, ") and (", x2, ", ", y2, ") is √", d, ", which evaluates to: ")
-        #Evalutes the square root
         dp2 = math.sqrt(d)
         print(dp2, "\n")
 
-    #Find midpoint given two points
+    #Find the midpoint given two points
     def midpoint():
-        #user defines points
-        x1, y1 = input("[Midpoint] What is the first set of points? Use format: \"x, y\"\n").split(", ")
+        x1, y1 = (input("[Midpoint] What is the first se2t of points? Use format: \"x, y\"\n").split(", "))
         x2, y2 = input("[Midpoint] What is the second set of points? Use format: \"x, y\"\n").split(", ")
         x1 = float(x1)
         y1 = float(y1)
         x2 = float(x2)
         y2 = float(y2)
-        # Find x of midpoint
+        #x of midpoint
         m1 = ((x1 + x2) / 2)
-        #Find y of midpoint
+        #y of midpoint
         m2 = ((y1 + y2) / 2)
-        #result:
         print("The midpoint of the points (", x1, ", ", y1, ") and (", x2, ", ", y2, ")\n", "M = (", m1, m2, ")\n")
-
-
 
     #Find point B given midpoint and point A
     def endpoint():
-        #user defines points
         x1, y1 = input("[Endpoint] What is the known endpoint? Use format: \"x, y\"\n").split(", ")
         m1, m2 = input("[Endpoint] What is the midpoint? Use format: \"x, y\"\n").split(", ")
         x1 = float(x1)
@@ -51,9 +43,9 @@ if __name__ == "__main__":
 
 while True:
     menunav = int(input("=======\n1. Find the DISTANCE between two points. \n2. Find the MIDPOINT of two points\n3. Find an ENDPOINT given midpoint and one endpoint\n Select a function: "))
-    if menunav == 1:
+    if menunav==1:
         distance()
-    elif menunav == 2:
+    elif menunav==2:
         midpoint()
-    elif menunav == 3:
+    elif menunav==3:
         endpoint()
