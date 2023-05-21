78d5f8ae1d49155206f0baa440c4d2ed3a6b8a47
DailyDoseofDogfood <76919179+DailyDoseofDogfood@users.noreply.github.com>
2023-05-20 22:22:41 -0400

    Add files via upload

--- distancemidpointcalculator.py
+++ distancemidpointcalculator.py
@@ -4,42 +4,42 @@ if __name__ == "__main__":
 
     #Find the distance between two points
     def distance():
-        x1, y1 = input("[Distance] What is the first set of points? Use format: \"x, y\"\n").split(", ")
-        x2, y2 = input("[Distance] What is the second set of points? Use format: \"x, y\"\n").split(", ")
-        x1 = float(x1)
-        y1 = float(y1)
-        x2 = float(x2)
-        y2 = float(y2)
-        d = ((x2-x1) ** 2) + ((y2-y1) ** 2)
-        print("The distance between (", x1, ", ", y1, ") and (", x2, ", ", y2, ") is √", d, ", which evaluates to: ")
+        point1_x, point1_y = input("[Distance] What is the first set of points? Use format: \"x, y\"\n").split(", ")
+        point2_x, point2_y = input("[Distance] What is the second set of points? Use format: \"x, y\"\n").split(", ")
+        point1_x = float(point1_x)
+        point1_y = float(point1_y)
+        point2_x = float(point2_x)
+        point2_y = float(point2_y)
+        d = ((point2_x-point1_x) ** 2) + ((point2_y-point1_y) ** 2)
+        print("The distance between (", point1_x, ", ", point1_y, ") and (", point2_x, ", ", point2_y, ") is √", d, ", which evaluates to: ")
         dp2 = math.sqrt(d)
         print(dp2, "\n")
 
     #Find the midpoint given two points
     def midpoint():
-        x1, y1 = (input("[Midpoint] What is the first se2t of points? Use format: \"x, y\"\n").split(", "))
-        x2, y2 = input("[Midpoint] What is the second set of points? Use format: \"x, y\"\n").split(", ")
-        x1 = float(x1)
-        y1 = float(y1)
-        x2 = float(x2)
-        y2 = float(y2)
+        point1_x, point1_y = (input("[Midpoint] What is the first se2t of points? Use format: \"x, y\"\n").split(", "))
+        point2_x, point2_y = input("[Midpoint] What is the second set of points? Use format: \"x, y\"\n").split(", ")
+        point1_x = float(point1_x)
+        point1_y = float(point1_y)
+        point2_x = float(point2_x)
+        point2_y = float(point2_y)
         #x of midpoint
-        m1 = ((x1 + x2) / 2)
+        midpoint_x = ((point1_x + point2_x) / 2)
         #y of midpoint
-        m2 = ((y1 + y2) / 2)
-        print("The midpoint of the points (", x1, ", ", y1, ") and (", x2, ", ", y2, ")\n", "M = (", m1, m2, ")\n")
+        midpoint_y = ((point1_y + point2_y) / 2)
+        print("The midpoint of the points (", point1_x, ", ", point1_y, ") and (", point2_x, ", ", point2_y, ")\n", "M = (", midpoint_x, midpoint_y, ")\n")
 
     #Find point B given midpoint and point A
     def endpoint():
-        x1, y1 = input("[Endpoint] What is the known endpoint? Use format: \"x, y\"\n").split(", ")
-        m1, m2 = input("[Endpoint] What is the midpoint? Use format: \"x, y\"\n").split(", ")
-        x1 = float(x1)
-        y1 = float(y1)
-        m1 = float(m1)
-        m2 = float(m2)
-        b1 = float((2*m1)-x1)
-        b2 = float((2*m2)-y1)
-        print("The missing endpoint 'B' from points M(", m1, ", ", m2, ") and A(", x1, ", ", y1, ") is: \n", "Point B = (", b1, b2, ")\n")
+        point1_x, point1_y = input("[Endpoint] What is the known endpoint? Use format: \"x, y\"\n").split(", ")
+        midpoint_x, midpoint_y = input("[Endpoint] What is the midpoint? Use format: \"x, y\"\n").split(", ")
+        point1_x = float(point1_x)
+        point1_y = float(point1_y)
+        midpoint_x = float(midpoint_x)
+        midpoint_y = float(midpoint_y)
+        b1 = float((2*midpoint_x)-point1_x)
+        b2 = float((2*midpoint_y)-point1_y)
+        print("The missing endpoint 'B' from points M(", midpoint_x, ", ", midpoint_y, ") and A(", point1_x, ", ", point1_y, ") is: \n", "Point B = (", b1, b2, ")\n")
 
 while True:
     menunav = int(input("=======\n1. Find the DISTANCE between two points. \n2. Find the MIDPOINT of two points\n3. Find an ENDPOINT given midpoint and one endpoint\n Select a function: "))
